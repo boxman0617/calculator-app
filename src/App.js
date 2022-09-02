e7142309f8edf38fd4d4ac39389addcd46f3474c
Alejandro <alejandro.rene.cervantes@gmail.com>
2022-09-01 23:40:47 -0700

    Added initial value box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [monthly, setMonthly] = useState("0");
   return (
     <div className="App">
       <header className="App-header">
         Mortgage Calculator
       </header>
+      <div className="Monthly-Input-Box">
+        <div className="Monthly-Input-Value">
+          {monthly}
+        </div>
+        <input type="text" value={monthly} onChange={(event) => {
+          setMonthly(event.target.value);
+        }}/>
+      </div>
     </div>
   );
 }
